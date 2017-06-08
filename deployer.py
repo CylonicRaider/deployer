a40b326f633f6bf6cd9be178dd9f3e449b440f4a
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-06-08 22:45:30 +0200

    Add argument parsing

--- deployer.py
+++ deployer.py
@@ -2,3 +2,16 @@
 # -*- coding: ascii -*-
 
 # deployer.py -- A pipe-controlled script runner.
+
+import argparse
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('-p', '--pipe', help='set control pipe location',
+                   default='/var/run/deployer', dest='pipe')
+    p.add_argument('-r', '--root', help='set script root location',
+                   default='/usr/share/deployer', dest='root')
+    res = p.parse_args()
+    print (res)
+
+if __name__ == '__main__': main()
